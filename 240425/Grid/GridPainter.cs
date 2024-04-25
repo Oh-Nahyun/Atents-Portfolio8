ba6ef0072553ab55b636bfb4a01069b3f7d81296
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-04-25 14:36:25 +0900

    Add files via upload

---
+++ 240425/Grid/GridPainter.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPainter : MonoBehaviour
+{
+    public GameObject line;
+    public GameObject letter;
+
+    //LineRenderer lineRenderer;
+
+    //private void Awake()
+    //{
+    //    lineRenderer = GetComponent<LineRenderer>();
+    //}
+
+    //private void Start()
+    //{
+    //    GetLine();
+    //}
+
+    //void GetLine()
+    //{
+    //    for (int x = 0; x < 11; x++)
+    //    {
+    //        //lineRenderer.SetPosition(x, new Vector3());
+    //        line.transform.position = new Vector3(x, 0.0f, 0.0f);
+    //        line.transform.Rotate(Vector3.zero);
+    //        Instantiate(line);
+    //        ResetLine();
+    //    }
+
+    //    for (int z = 0; z < 11; z++)
+    //    {
+    //        line.transform.position = new Vector3(0.0f, 0.0f, z);
+    //        line.transform.Rotate(0.0f, 90.0f, 0.0f);
+    //        Instantiate(line);
+    //        ResetLine();
+    //    }
+    //}
+
+    //void ResetLine()
+    //{
+    //    line.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+    //    line.transform.Rotate(Vector3.zero);
+    //}
+
+    //void GetLetter()
+    //{
+
+    //}
+}
