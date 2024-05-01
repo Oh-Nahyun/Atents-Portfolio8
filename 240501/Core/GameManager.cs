d5636ac14192c853c4cd5175033af76b41bf9cb6
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-05-01 13:59:03 +0900

    Add files via upload

---
+++ 240501/Core/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    CinemachineImpulseSource cameraImpulseSource;
+
+    protected override void OnPreInitialize()
+    {
+        base.OnPreInitialize();
+
+        cameraImpulseSource = GetComponentInChildren<CinemachineImpulseSource>();
+    }
+
+    public void CameraShake(float force)
+    {
+        cameraImpulseSource.GenerateImpulseWithVelocity(force * Random.insideUnitCircle.normalized);
+    }
+}
