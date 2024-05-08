08ea73b404731ffe7bfc1ea2f7db3b37c41fad0e
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-05-08 13:07:04 +0900

    Add files via upload

---
+++ 240508/Core/GameManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    /// <summary>
+    /// 사용자 플레이어 (왼쪽)
+    /// </summary>
+    UserPlayer user;
+    public UserPlayer UserPlayer => user;
+
+    /// <summary>
+    /// 적 플레이어 (오른쪽)
+    /// </summary>
+    EnemyPlayer enemy;
+    public EnemyPlayer EnemyPlayer => enemy;
+
+    /// <summary>
+    /// 카메라 진동 소스
+    /// </summary>
+    CinemachineImpulseSource cameraImpulseSource;
+
+    /// <summary>
+    /// 턴 매니저
+    /// </summary>
+    TurnManager turnManager;
+
+    public TurnManager TurnManager => turnManager;
+
+    /// <summary>
+    /// 테스트 모드인지 아닌지 표시용 변수
+    /// </summary>
+    public bool IsTestMode = false;
+
+    protected override void OnPreInitialize()
+    {
+        base.OnPreInitialize();
+
+        cameraImpulseSource = GetComponentInChildren<CinemachineImpulseSource>();
+    }
+
+    protected override void OnInitialize()
+    {
+        user = FindAnyObjectByType<UserPlayer>();
+        enemy = FindAnyObjectByType<EnemyPlayer>();
+
+        turnManager = GetComponent<TurnManager>();
+        turnManager.OnInitialize(user, enemy);
+    }
+
+    public void CameraShake(float force)
+    {
+        cameraImpulseSource.GenerateImpulseWithVelocity(force * Random.insideUnitCircle.normalized);
+    }
+}
---
+++ 240508/Grid/GridPainter.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GridPainter : MonoBehaviour
+{
+    public GameObject linePrefab;
+    public GameObject letterPrefab;
+    const int gridLineCount = 11;
+
+    private void Awake()
+    {
+        DrawGridLines();
+        DrawGridLetter();
+    }
+
+    void DrawGridLines()
+    {
+        // 세로선 그리기
+        for (int i = 0; i < gridLineCount; i++)
+        {
+            GameObject line = Instantiate(linePrefab, transform); ///// 마지막에 transform을 넣는 이유 : 부모 설정
+            LineRenderer lineRenderer = line.GetComponent<LineRenderer>();
+            lineRenderer.SetPosition(0, new Vector3(i, 0, 1));
+            lineRenderer.SetPosition(1, new Vector3(i, 0, 1 - gridLineCount));
+        }
+
+        // 가로선 그리기
+        for (int i = 0; i < gridLineCount; i++)
+        {
+            GameObject line = Instantiate(linePrefab, transform);
+            LineRenderer lineRenderer = line.GetComponent<LineRenderer>();
+            lineRenderer.SetPosition(0, new Vector3(-1, 0, -i));
+            lineRenderer.SetPosition(1, new Vector3(gridLineCount - 1, 0, -i));
+        }
+    }
+
+    void DrawGridLetter()
+    {
+        // 가로로 알파벳 찍기
+        for (int i = 1; i < gridLineCount; i++)
+        {
+            GameObject letter = Instantiate(letterPrefab, transform);
+            letter.transform.position = new Vector3(i - 0.5f, 0, 0.5f);
+            TextMeshPro text = letter.GetComponent<TextMeshPro>();
+            char alphabet = (char)('A' + i - 1);
+            text.text = alphabet.ToString();
+        }
+
+        // 세로로 숫자 찍기
+        for (int i = 1; i < gridLineCount; i++)
+        {
+            GameObject letter = Instantiate(letterPrefab, transform);
+            letter.transform.position = new Vector3(-0.5f, 0, 0.5f - i);
+            TextMeshPro text = letter.GetComponent<TextMeshPro>();
+            text.text = i.ToString();
+            if (i > 9)
+            {
+                text.fontSize = 8;
+            }
+        }
+    }
+}
+
+/// 실습_240425
+/// GridPainter 구현하기
+
+/* // 내가 작성해본 코드
+LineRenderer lineRenderer;
+
+private void Awake()
+{
+    lineRenderer = GetComponent<LineRenderer>();
+}
+
+private void Start()
+{
+    GetLine();
+}
+
+void GetLine()
+{
+    for (int x = 0; x < 11; x++)
+    {
+        //lineRenderer.SetPosition(x, new Vector3());
+        line.transform.position = new Vector3(x, 0.0f, 0.0f);
+        line.transform.Rotate(Vector3.zero);
+        Instantiate(line);
+        ResetLine();
+    }
+
+    for (int z = 0; z < 11; z++)
+    {
+        line.transform.position = new Vector3(0.0f, 0.0f, z);
+        line.transform.Rotate(0.0f, 90.0f, 0.0f);
+        Instantiate(line);
+        ResetLine();
+    }
+}
+
+void ResetLine()
+{
+    line.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+    line.transform.Rotate(Vector3.zero);
+}
+
+void GetLetter()
+{
+
+}
+*/
