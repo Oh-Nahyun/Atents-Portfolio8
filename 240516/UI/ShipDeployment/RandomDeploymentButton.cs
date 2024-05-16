a9ed8a6fa9c2580ec903772c84e0f346f8b220cb
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2024-05-16 13:16:53 +0900

    Add files via upload

---
+++ 240516/UI/ShipDeployment/RandomDeploymentButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomDeploymentButton : MonoBehaviour
+{
+    // 아직 배치되지 않은 모든 함선을 자동으로 배치
+
+    private void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        UserPlayer player = GameManager.Instance.UserPlayer;
+        if (player.IsAllDeployed)
+        {
+            // 함선이 전부 배치되어있는 상태이면, 전부 배치 취소해서 새로 배치하게 만들기
+            player.UndoAllShipDeployment();
+        }
+        player.AutoShipDeployment(true);
+    }
+}
